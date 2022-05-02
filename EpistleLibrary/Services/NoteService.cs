5c384bf95e6db903bf7e3242411e99d0dba68cf6
tehtko <moderndsnsam@gmail.com>
2022-05-02 16:57:45 -0400

    Create NoteService

---
+++ EpistleLibrary/Services/NoteService.cs
@@ -0,0 +1,19 @@
+﻿using EpistleLibrary.Models;
+
+namespace EpistleLibrary.Services
+{
+    public class NoteService
+    {
+        public static List<NoteModel> GetNotes()
+        {
+            List<NoteModel> notes = new List<NoteModel>();
+
+            return notes;
+        }
+
+        public static void AddNote(NoteModel note)
+        {
+
+        }
+    }
+}
