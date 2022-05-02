d1c872d95793445c2e4d61e60109743ffa6540d1
tehtko <moderndsnsam@gmail.com>
2022-05-01 21:38:07 -0400

    Add class library

---
+++ EpistleLibrary/Models/NoteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EpistleLibrary.Models
+{
+    public class NoteModel
+    {
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public string Content { get; set; }
+    }
+}
---
+++ EpistleLibrary/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EpistleLibrary.Models
+{
+    public class UserModel
+    {
+        public int Username { get; set; }
+        public int Password { get; set; }
+        public List<NoteModel> Notes { get; set; }
+    }
+}
