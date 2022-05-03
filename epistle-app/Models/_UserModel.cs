0871d6ef670d406156608fee6648a3de816ba1f0
tehtko <moderndsnsam@gmail.com>
2022-05-03 16:25:37 -0400

    Make fields nullable

--- epistle-app/Models/_UserModel.cs
+++ epistle-app/Models/_UserModel.cs
@@ -7,16 +7,16 @@ namespace epistle_app.Models
         [Display(Name = "Username")]
         [Required(ErrorMessage = "Please provide a valid Username")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters.")]
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
         [Display(Name = "Password")]
         [Required(ErrorMessage = "Please provide a valid password")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
     }
 }
