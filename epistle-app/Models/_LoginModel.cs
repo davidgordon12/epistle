de755c26692ee816a577f022455238c3db54f87f
tehtko <moderndsnsam@gmail.com>
2022-05-03 16:25:28 -0400

    Create login Model

---
+++ epistle-app/Models/_LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epistle_app.Models
+{
+    public class _LoginModel
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Please provide a valid Username")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters.")]
+        public string? Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please provide a valid password")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
