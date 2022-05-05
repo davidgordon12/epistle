105fc2f8d10e1a11cf2fc9d68c7bf0c1a8cc4514
tehtko <moderndsnsam@gmail.com>
2022-05-05 17:05:06 -0400

    Configure sessions

--- epistle-app/Program.cs
+++ epistle-app/Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromSeconds(3600);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +25,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
