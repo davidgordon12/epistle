67ae8b32589d39750ae85440c8e0496d46e3c0d8
tehtko <moderndsnsam@gmail.com>
2022-05-04 22:00:12 -0400

    Add account creation

--- EpistleLibrary/Services/UserService.cs
+++ EpistleLibrary/Services/UserService.cs
@@ -1,15 +1,38 @@
-﻿namespace EpistleLibrary.Services
+﻿using Microsoft.EntityFrameworkCore;
+using EpistleLibrary.Data;
+using EpistleLibrary.Models;
+
+namespace EpistleLibrary.Services
 {
     public class UserService
     {
-        public static string GetUser()
+        public static string GetUser(string username, string password)
         {
-            return "David Gordon";
+
+
+            return "David";
         }
 
-        public static bool CreateUser()
+        public static bool CreateUser(string username, string password)
         {
-            return true;
+            try
+            {
+                EpistleContext context = new();
+
+                context.Add(new UserModel
+                {
+                    Username = username,
+                    Password = password
+                });
+
+                context.SaveChanges();
+
+                return true;
+            } 
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public static bool DeleteUser()
