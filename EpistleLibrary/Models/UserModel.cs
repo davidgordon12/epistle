8857983f753b00f725d5d316a785514695ac789d
tehtko <moderndsnsam@gmail.com>
2022-05-04 20:41:43 -0400

    Create primary key [username]

--- EpistleLibrary/Models/UserModel.cs
+++ EpistleLibrary/Models/UserModel.cs
@@ -1,8 +1,10 @@
-﻿namespace EpistleLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EpistleLibrary.Models
 {
     public class UserModel
     {
-        public int Id { get; set; } 
+        [Key]
         public string Username { get; set; }
         public int Password { get; set; }
         public List<NoteModel>? Notes { get; set; }
