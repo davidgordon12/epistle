860eb27987d7bf4488defc6214d71b5bf99341a8
tehtko <moderndsnsam@gmail.com>
2022-05-04 20:19:03 -0400

    Add foreign key

--- EpistleLibrary/Models/NoteModel.cs
+++ EpistleLibrary/Models/NoteModel.cs
@@ -2,8 +2,11 @@
 {
     public class NoteModel
     {
-        public string? Title { get; set; }
+        public string Title { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
-        public string? Content { get; set; }
+        public string Content { get; set; }
+
+        public int UserId { get; set; }
+        public UserModel User { get; set; }
     }
 }
