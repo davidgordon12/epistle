487258c5c5872102dc45e4a22057a372a06ec26b
tehtko <moderndsnsam@gmail.com>
2022-05-04 20:45:42 -0400

    Change foreign key

--- EpistleLibrary/Models/NoteModel.cs
+++ EpistleLibrary/Models/NoteModel.cs
@@ -7,7 +7,7 @@
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public string Content { get; set; }
 
-        public int UserId { get; set; }
+        public string Username { get; set; }
         public UserModel User { get; set; }
     }
 }
