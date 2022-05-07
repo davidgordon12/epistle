296fe315167c3ae47423dc453f54e0d998258c5a
tehtko <moderndsnsam@gmail.com>
2022-05-06 21:44:14 -0400

    Make email field string with regex

--- EpistleLibrary/Models/UserModel.cs
+++ EpistleLibrary/Models/UserModel.cs
@@ -6,6 +6,7 @@ namespace EpistleLibrary.Models
     {
         [Key]
         public string Username { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public List<NoteModel>? Notes { get; set; }
     }
