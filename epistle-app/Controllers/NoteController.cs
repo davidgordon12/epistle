f84331becb8d0742ab66200e7be53e193b0f94be
tehtko <moderndsnsam@gmail.com>
2022-05-08 21:32:55 -0400

    Add CreateNote ActionResult

--- epistle-app/Controllers/NoteController.cs
+++ epistle-app/Controllers/NoteController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EpistleLibrary.Models;
+using EpistleLibrary.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace epistle_app.Controllers
 {
@@ -6,7 +9,31 @@ namespace epistle_app.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var user = JsonConvert.DeserializeObject<UserModel>(HttpContext.Session.GetString("Session"));
+            ViewBag.Username = user.Username;
+
+            return View(new NoteModel
+            {
+                Content = ""
+            });
+        }
+
+        [HttpPost]
+        public IActionResult CreateNote(NoteModel note)
+        {
+            var user = JsonConvert.DeserializeObject<UserModel>(HttpContext.Session.GetString("Session"));
+            note.User = JsonConvert.DeserializeObject<UserModel>(HttpContext.Session.GetString("Session"));
+
+            if (note.Content == "" || note.Content is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            NoteService.AddNote(note);
+
+            ViewBag.Username = user.Username;
+
+            return RedirectToAction("Index");
         }
     }
 }
