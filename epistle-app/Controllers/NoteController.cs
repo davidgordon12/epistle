b10e282c9d56ac7ef39d5b8586c0a5ad055cca9c
tehtko <moderndsnsam@gmail.com>
2022-05-08 21:29:45 -0400

    Create note controller

---
+++ epistle-app/Controllers/NoteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace epistle_app.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
