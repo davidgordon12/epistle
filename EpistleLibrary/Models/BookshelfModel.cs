a380400e0287ba8575892472c5f7890a1e042bb2
tehtko <moderndsnsam@gmail.com>
2022-05-10 00:27:09 -0400

    Update BookshelfModel

--- EpistleLibrary/Models/BookshelfModel.cs
+++ EpistleLibrary/Models/BookshelfModel.cs
@@ -7,7 +7,5 @@ namespace EpistleLibrary.Models
         [Key]
         public string Title { get; set; }
         public List<NoteModel> Notes { get; set; }
-
-        public UserModel User { get; set; }
     }
 }
