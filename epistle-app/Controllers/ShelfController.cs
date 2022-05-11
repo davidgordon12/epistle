eda547b55409979f663828d9385b79ff4c1fe4ea
tehtko <moderndsnsam@gmail.com>
2022-05-10 22:04:53 -0400

    Create ShelfController

---
+++ epistle-app/Controllers/ShelfController.cs
@@ -0,0 +1,35 @@
+﻿using EpistleLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace epistle_app.Controllers
+{
+    public class ShelfController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = JsonConvert.DeserializeObject<UserModel>(HttpContext.Session.GetString("Session"));
+            ViewBag.Username = user.Username;
+
+            return View(new NoteModel
+            {
+                Content = ""
+            });
+        }
+
+        public IActionResult CreateShelf(BookshelfModel bookshelf)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult AddShelf(BookshelfModel bookshelf)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult LoadShelf(BookshelfModel bookshelf)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
