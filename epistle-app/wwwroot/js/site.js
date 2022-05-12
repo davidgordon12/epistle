09f6d39896113945cb56de81575d2565f2048058
tehtko <moderndsnsam@gmail.com>
2022-05-12 16:44:18 -0400

    Move js methods to site.js

--- epistle-app/wwwroot/js/site.js
+++ epistle-app/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿function dark_mode() {
+    document.getElementById('textarea').classList.toggle('dark_mode');
+}
+function render_shelf() {
+    document.getElementById('shelf_creation').classList.toggle('visually-hidden');
+}
+function render_push() {
+    document.getElementById('push_shelf').classList.toggle('visually-hidden');
+}
+function fix_text() {
+    setTimeout(fix, 100);
+}
+function fix() {
+    document.getElementById('textarea').value = document.getElementById('textarea').textContent.replace("&#xD;&#xA;", "");
+}
