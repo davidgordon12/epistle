dac031b52f68f923d1173914ab0ccd73dd136b07
tehtko <moderndsnsam@gmail.com>
2022-05-16 17:39:09 -0400

    Hamburger menu

--- epistle-app/wwwroot/js/site.js
+++ epistle-app/wwwroot/js/site.js
@@ -5,22 +5,34 @@
     }
 }
 
+function show_shelf() {
+
+}
+
+function show_notes() {
+
+}
 
 function dark_mode() {
     document.getElementById('textarea').classList.toggle('dark_mode');
 }
+
 function render_shelf() {
     document.getElementById('shelf_creation').classList.toggle('visually-hidden');
 }
+
 function render_push() {
     document.getElementById('push_shelf').classList.toggle('visually-hidden');
 }
+
 function render_delete() {
     document.getElementById('delete_shelf').classList.toggle('visually-hidden');
 }
+
 function fix_text() {
     setTimeout(fix, 100);
 }
+
 function fix() {
     document.getElementById('textarea').value = document.getElementById('textarea').textContent.replace("&#xD;&#xA;", "");
 }
